2ea8c0357f0f2250575051e5cebc70d991dab916
Ebru Seiwert <ebru.blue@gmail.com>
2024-04-12 16:31:34 -0500

    CardView -> base

--- Hello World/HelloWorldView.swift
+++ Hello World/HelloWorldView.swift
@@ -23,15 +23,17 @@ struct CardView: View {
     var isFaceUp: Bool = false
             
         var body: some View {
-            ZStack (content: {
+            ZStack {
+                var base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
+                
                 if isFaceUp {
-                    RoundedRectangle(cornerRadius: 12).foregroundColor(.white)
-                    RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 2)
+                    base.fill(.white)
+                    base.strokeBorder(lineWidth: 2)
                     Text("🌞").font(.largeTitle)
                 } else {
-                    RoundedRectangle(cornerRadius: 12)
+                    base.fill()
                         }
-            })
+            }
     }
 }
 #Preview {
