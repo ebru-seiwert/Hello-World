62684bfff482b118b3b6eca1f9c2baa2f52c30d7
Ebru Seiwert <ebru.blue@gmail.com>
2024-04-12 10:32:05 -0500

    Initial Commit

---
+++ Hello World/Hello_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_WorldApp.swift
+//  Hello World
+//
+//  Created by Ebru Seiwert on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Hello_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
